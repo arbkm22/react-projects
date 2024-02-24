8c0293b4b26cf022e1329fdc6873d1070908a38c
Bhaskar Mahto <arbkm22@gmail.com>
2024-02-24 09:11:31 +0530

    take user input and display the tasks and some css changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
 
     const handleSubmit = (e) => {
-        e.preventDefaults();
+        e.preventDefault();
+        if (inputValue.trim() !== '') {
+            setTasks([...tasks, inputValue]);
+            setInputValue('');
+        }
+        console.log('inputValue: ', inputValue);
+        console.log('tasks: ', tasks);
     }
 
     const handleChange = (e) => {
-        let inputValue = e.target.value;
-        console.log('inputVal: ', inputValue)
+        let inpVal = e.target.value;
+        setInputValue(inpVal);
     }
+
+    const [inputValue, setInputValue] = useState("");
+    const [tasks, setTasks] = useState([]);
 
     return (
         <div className="App">
             <div className="todo">
                 <p className="heading">To Do List</p>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} >
                     <input
                         name="item"
                         placeholder="Add New"
                         className="input-box" 
+                        value={inputValue}
                         onChange={handleChange} />
                 </form>
+                <div className="added-items">
+                    <ul>
+                        {tasks.map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
