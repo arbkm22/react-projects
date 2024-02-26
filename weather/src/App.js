1b25e14acb2ae146bc9e23aa181cf5f000ff936f
Bhaskar Mahto <arbkm22@gmail.com>
2024-02-26 22:43:43 +0530

    added input field changed layout

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-        <p>Weather App</p>
-    </div>
-  );
+
+    const [inputValue, setInputValue] = useState("");
+    const [city, setCity] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setCity(inputValue);
+        console.log('inputValue: ', inputValue);
+        setInputValue('');
+    }
+
+    return (
+        <div className="App">
+            <form 
+                onSubmit={handleSubmit}
+                className="input">
+                <input
+                    name="item"
+                    placeholder="Enter a city..."
+                    className="input-box"
+                    value={inputValue}
+                    autoComplete="off"
+                    onChange={(e) => setInputValue(e.target.value)} />
+            </form>
+            <div className="weather">
+                <p>Weather App</p>
+                <div className="main">
+                    <div className="display">
+                        {city}
+                    </div>  
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
