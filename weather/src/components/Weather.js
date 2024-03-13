ca63f353a855f734e9c4484091a4f991c38bee7e
Bhaskar Mahto <arbkm22@gmail.com>
2024-03-13 21:26:53 +0530

    trying to add different weather icon as openweather is ugly

--- weather/src/components/Weather.js
+++ weather/src/components/Weather.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Rain from '../assets/images/Rain.svg';
+import Drizzle from '../assets/images/Drizzle.svg';
+import Clouds from '../assets/images/Clouds.svg';
+import Clear from '../assets/images/Clear.svg';
+import Snow from '../assets/images/Snow.svg';
+import Thunderstorm from '../assets/images/Thunderstorm.svg';
+
 
 function Weather(props) {
 
@@ -7,38 +14,52 @@ function Weather(props) {
     const day = now.getDay();
     const currentTime = new Date().toLocaleTimeString();
 
+    let icon;
+    switch (props.weatherIcon) {
+        case 'Rain':
+            icon = <Rain />
+        case 'Drizzle':
+            icon = <Drizzle />
+        case 'Clouds':
+            icon = <Clouds />
+        case 'Clear':
+            icon = <Clear />
+        case 'Snow':
+            icon = <Snow />
+        case 'Thunderstorm':
+            icon = <Thunderstorm />
+    }
+
     return (
         <div className="main">
-            <div className="display">
-                {props.data && props.data.map((item, index) => (
-                    <div 
-                        key={index}
-                        className='weather-data'
-                    >
-                        <div className="first-row">
-                            <img 
-                                src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`}
-                                className="weather-icon"
-                                alt=""
-                            ></img>
-                            <div className="weather-stat">{item.main.temp}{'\u00b0'}C</div>
-                            <div className="left-items">
-                                <div>Humidity: {item.main.humidity}%</div>
-                                <div>Wind: {Math.round(item.wind.speed * (18/5))} km/h</div>
-                            </div>
+            {props.data && props.data.map((item, index) => (
+                <div 
+                    key={index}
+                    className='weather-data'
+                >
+                    <div className="first-row">
+                        <img 
+                            src={icon}
+                            className="weather-icon"
+                            alt=""
+                        ></img>
+                        <div className="weather-stat">{item.main.temp}{'\u00b0'}C</div>
+                        <div className="left-items-container">
+                            <div>Humidity: {item.main.humidity}%</div>
+                            <div>Wind: {Math.round(item.wind.speed * (18/5))} km/h</div>
                         </div>
-                        <div className="second-row">
-                            Weather
-                            {
-                                <div className="right-items">
-                                    <p>{days[day]}, {currentTime}</p>
-                                    <p>{(item.weather[0].description)}</p>
-                                </div>
-                            }
-                        </div> 
                     </div>
-                ))}
-            </div>
+                    <div className="second-row">
+                        Weather
+                        {
+                            <div className="right-items">
+                                <p>{days[day]}, {currentTime}</p>
+                                <p>{(item.weather[0].description)}</p>
+                            </div>
+                        }
+                    </div> 
+                </div>
+            ))}
         </div>
     );
 }
